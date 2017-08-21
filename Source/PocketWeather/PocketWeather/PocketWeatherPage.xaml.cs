b6084919f330d6b0cee0a439bc3def2e435e8d75
QuanP <mr.pquan@gmail.com>
2017-08-21 20:17:45 +0700

    init project

---
+++ Source/PocketWeather/PocketWeather/PocketWeatherPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PocketWeather
+{
+    public partial class PocketWeatherPage : ContentPage
+    {
+        public PocketWeatherPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
